1127c27b4db8cacd6574d5ddcb98aadc0cde80f3
mariano7736 <mariano7736@gmail.com>
2024-05-20 20:00:41 -0300

    animacion de texto

--- trabajo JS 2024/script/script.js
+++ trabajo JS 2024/script/script.js
@@ -399,60 +399,63 @@ document.querySelector('.scroll-right').addEventListener('click', () => {
 });
 
 /* texto animado*/
+
 class TextoAnimado {
-    constructor(id) {
-        this.texto = document.getElementById(id);
-        this.letras = this.texto.innerText.split("");
+	constructor(id, objetivo) {
+		this.texto = document.getElementById(id);
+		this.objetivo = document.getElementById(objetivo);
+		this.letras = this.texto.innerText.split("");
 
-        this.texto.innerText = '';
+		this.texto.innerText = '';
 
-        this.letras.forEach((letra) => {
-            let caracter = letra === ' ' ? '&nbsp;' : letra;
+		this.letras.forEach((letra) => {
+			let caracter = letra === ' ' ? '&nbsp;' : letra;
 
-            this.texto.innerHTML = this.texto.innerHTML + `
-                <div>
-                    <span>${caracter}</span>
-                    <span class="segunda-linea">${caracter}</span>
-                </div>
-            `;
-        });
+			this.texto.innerHTML = this.texto.innerHTML + `
+				<div>
+					<span>${caracter}</span>
+					
+				</div>
+			`;
+		});
 
-        this.iniciarAnimacion();
-    }
 
-    iniciarAnimacion() {
-        const duracionAnimacion = 100; // Duración de la animación en milisegundos
-        let cuenta = 0;
 
-        const animar = () => {
-            const intervalo = setInterval(() => {
-                if (cuenta < this.texto.children.length) {
-                    this.texto.children[cuenta].classList.add('animacion');
-                    cuenta += 1;
-                } else {
-                    clearInterval(intervalo);
-                    // Reiniciar la animación después de un breve retraso
-                    setTimeout(() => {
-                        cuenta = 0;
-                        this.texto.innerHTML = ''; // Limpiar el contenido para reiniciar la animación
-                        this.letras.forEach((letra) => {
-                            let caracter = letra === ' ' ? '&nbsp;' : letra;
+		setInterval(() => {
 
-                            this.texto.innerHTML = this.texto.innerHTML + `
-                                <div>
-                                    <span>${caracter}</span>
-                                    
-                                </div>
-                            `;
-                        });
-                        animar();
-                    }, duracionAnimacion);
-                }
-            }, duracionAnimacion);
-        };
 
-        animar();
-    }
+
+			let cuenta = 0;
+
+			const intervalo = setInterval(() => {
+				if (cuenta < this.texto.children.length) {
+					this.texto.children[cuenta].classList.add('animacion');
+					cuenta += 1;
+				} else {
+					clearInterval(intervalo);
+				}
+			}, 30);
+
+		},3000);
+
+
+
+		setInterval(() => {
+			let cuenta = 0;
+
+			const intervalo = setInterval(() => {
+				if (cuenta < this.texto.children.length) {
+					this.texto.children[cuenta].classList.remove('animacion');
+					cuenta += 1;
+				} else {
+					clearInterval(intervalo);
+				}
+			}, 30);
+
+		} ,6000);
+
+
+	}
 }
 
-new TextoAnimado('logo');
+new TextoAnimado('logo', 'logotipo');
